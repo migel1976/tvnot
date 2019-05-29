4f5b72d2b1b5127aa0e866e5410f5b144275d769
Michael Smirnov <msmirnov@ip-172-31-14-87.us-east-2.compute.internal>
2019-05-29 13:28:34 +0000

    initial checking

---
+++ minutnik.py
@@ -0,0 +1,72 @@
+import sys, glob, os, time, pandas as pd
+
+def tail_file_events(fn, offset, buf_len):
+    found = False
+    with open(fn, "r") as fd:
+        fd.seek(offset, os.SEEK_SET)
+        buf = fd.read(buf_len).lower()
+        for keyword in ['nb:', 'error', 'exception']:
+            if keyword in buf:
+                found = True
+                break
+
+    return found
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print >>sys.stderr, "Usage: python %s <log-dir>" % sys.argv[0]
+        sys.exit(2)
+    dir_ = os.path.expanduser(sys.argv[1])
+    dir_glob_mask = os.path.join(dir_, "*.log")
+    dir_listing_fn = os.path.join(dir_, "listing.csv")
+    max_buf_len = 50 * 1024
+    max_fsz_growth_speed = 30 * 1024
+    now = time.time()
+
+    old_f_listing = pd.read_csv(dir_listing_fn).set_index("fname") if os.path.exists(dir_listing_fn) else None
+    files = glob.glob(dir_glob_mask)
+
+    events = {}
+    f_listing = []
+    if isinstance(old_f_listing, type(None)):
+        events[None] = "listing file created: " + dir_listing_fn
+
+    for file_ in files:
+        st = os.stat(file_)
+        fsize = st.st_size
+        fmtime = int(st.st_mtime)
+        f_listing.append((file_, fsize, fmtime))
+        del st
+
+        if isinstance(old_f_listing, type(None)):
+            continue
+
+        if not file_ in old_f_listing.index:
+            found = tail_file_events(file_, 0, max_buf_len)
+            if found: events[file_] = "something interesting found"
+        else:
+            old_size = old_f_listing.loc[file_, 'fsize']
+            old_mtime = old_f_listing.loc[file_, 'fmtime']
+            if fsize == old_size:
+                if fmtime == old_mtime:
+                    pass
+                else:
+                    events[file_] = "no size change but mtime changed"
+            else:
+                if fsize < old_size:
+                    events[file_] = "file shrinks"
+                else:
+                    if now != old_mtime and (fsize - old_size) / (now - old_mtime) > max_fsz_growth_speed:
+                        events[file_] = "file grows too fast"
+                    else:
+                        found = tail_file_events(file_, old_size, min(fsize - old_size, max_buf_len))
+                        if found: events[file_] = "something interesting found"
+
+    if len(events):
+        for file_, event_desc in events.items():
+            print file_, ":", event_desc
+
+    pd.DataFrame.from_records(f_listing, columns = ['fname', 'fsize', 'fmtime']).to_csv(dir_listing_fn, index = False)
+    sys.exit(0)
+    
+        
